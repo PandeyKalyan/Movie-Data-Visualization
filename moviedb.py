aff8333934c59fa2f0b1576ba48f2a51adb9c655
PandeyKalyan <pandey.kalyan416@gmail.com>
2016-06-19 22:11:47 +0545

    Add Tables

--- moviedb.py
+++ moviedb.py
@@ -13,12 +13,36 @@ DROP TABLE IF EXISTS Movies''')
 cur.execute('''
 DROP TABLE IF EXISTS Movies_Actors''')
 
+cur.execute('''
+DROP TABLE IF EXISTS Directors''')
+
+cur.execute('''
+DROP TABLE IF EXISTS Genre''')
+
+cur.execute('''
+DROP TABLE IF EXISTS Movies_Genre''')
+
+
 cur.execute('''CREATE TABLE Movies (
+    id  INTEGER NOT NULL PRIMARY KEY UNIQUE,
+    name    TEXT UNIQUE,
+    year    INTEGER,
+    runtime TEXT,
+    director_id INTEGER,
+    rating FLOAT
+)''')
+
+cur.execute('''CREATE TABLE Actors (
     id  INTEGER NOT NULL PRIMARY KEY UNIQUE,
     name    TEXT UNIQUE
 )''')
 
-cur.execute('''CREATE TABLE Actors (
+cur.execute('''CREATE TABLE Directors (
+    id  INTEGER NOT NULL PRIMARY KEY UNIQUE,
+    name    TEXT UNIQUE
+)''')
+
+cur.execute('''CREATE TABLE Genre (
     id  INTEGER NOT NULL PRIMARY KEY UNIQUE,
     name    TEXT UNIQUE
 )''')
@@ -29,16 +53,34 @@ cur.execute('''CREATE TABLE Movies_Actors (
     PRIMARY KEY (movie_id, actor_id)
 )''')
 
+cur.execute('''CREATE TABLE Movies_Genre (
+    movie_id  INTEGER,
+    genre_id  INTEGER,
+    PRIMARY KEY (movie_id, genre_id)
+)''')
+
 fh = open("movie_list.txt")
 for line in fh:
     movie = line.strip()
     movie_data = scraper.imdb_scraper(movie)
     actors = movie_data['Actors'].split(', ')
+    genres = movie_data['Genre'].split(', ')
+    title = movie_data['Title']
+    year = int(movie_data['Year'])
+    runtime = movie_data['Runtime']
+    director = movie_data['Director']
+    rating = float(movie_data['imdbRating'])
 
-    cur.execute('''INSERT OR IGNORE INTO Movies (name)
-        VALUES ( ? )''', (movie_data['Title'], ))
+    cur.execute('''INSERT OR IGNORE INTO Directors (name)
+        VALUES ( ? )''', (director, ))
+    cur.execute('SELECT id FROM Directors WHERE name = ? ',
+                (director, ))
+    director_id = cur.fetchone()[0]
+
+    cur.execute('''INSERT OR IGNORE INTO Movies (name, year, runtime, director_id, rating)
+        VALUES ( ?, ?, ?, ?, ? )''', (title, year, runtime, director_id, rating))
     cur.execute('SELECT id FROM Movies WHERE name = ? ',
-                (movie_data['Title'], ))
+                (title, ))
     movie_id = cur.fetchone()[0]
 
     for actor in actors:
@@ -50,5 +92,15 @@ for line in fh:
         cur.execute('''INSERT OR REPLACE INTO Movies_Actors
         (movie_id, actor_id) VALUES (?, ?)''',
                     (movie_id, actor_id))
+
+    for genre in genres:
+        cur.execute('''INSERT OR IGNORE INTO Genre (name)
+        VALUES ( ? )''', (genre, ))
+        cur.execute('SELECT id FROM Genre WHERE name = ? ', (genre, ))
+        genre_id = cur.fetchone()[0]
+
+        cur.execute('''INSERT OR REPLACE INTO Movies_Genre
+        (movie_id, genre_id) VALUES (?, ?)''',
+                    (movie_id, genre_id))
 
 conn.commit()
